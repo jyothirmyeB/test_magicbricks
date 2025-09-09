799c6bd5bb66f729ead8454981cd94fa1762d28e
jyothirmyeB <jyothirmyebussa@gmail.com>
2025-09-09 18:28:17 +0530

    Day 4: First commit - Outcome:Shortlisting property successful

--- src/test/java/com/objectrepository/Locators.java
+++ src/test/java/com/objectrepository/Locators.java
@@ -15,14 +15,28 @@ public class Locators {
 	 public static By otp4 = By.id("verify04");
      public static By continuebtn = By.xpath("//*[@id=\"verifyOtpDiv\"]/div[2]/div[3]/button");
      public static By closepopup = By.xpath("//*[@id=\"userOnboardingWidget\"]/div/div[1]");
+    
      public static By enterlocation = By.id("keyword");
      public static By clickPropertType = By.id("propType_buy");
+     public static By clearPropertyType = By.xpath("//*[@id=\"propType_buy\"]/div[2]/div/div/div[1]/div[2]/div[4]");
      public static By dropdownProperType = By.xpath("//*[@id=\"propType_buy\"]/div[1]");
      public static By selectPropertyType = By.id("10002_10003_10021_10022");
-     //public static By clickBudget = By.id("rent_budget_lbl");
-     public static By dropdownBudget = By.xpath("//*[@id=\"searchFormHolderSection\"]/section/div/div[1]/div[3]/div[3]/div[1]");
-     public static By minPrice = By.xpath("//*[@id=\"minBudjet\"]/div[10]");
-     public static By maxPrice = By.xpath("//*[@id=\"maxBhkIndex_11\"]");
+     public static By closePropertyType = By.xpath("//*[@id=\"buy_proertyTypeDefault\"]");
+     
+     public static By clickBudget = By.id("rent_budget_lbl");
+     //public static By dropdownBudget = By.xpath("//*[@id=\"searchFormHolderSection\"]/section/div/div[1]/div[3]/div[3]/div[1]");
+     public static By minPrice = By.xpath("//*[@id=\"budgetMin\"]");
+     public static By maxPrice = By.xpath("//*[@id=\"budgetMax\"]");
+     public static By closeBudget = By.xpath("//*[@id=\"rent_budget_lbl\"]");
      public static By searchButton = By.xpath("//*[@id=\"searchFormHolderSection\"]/section/div/div[1]/div[3]/div[4]");
      public static By sortBy = By.xpath("//*[@id=\"body\"]/div[5]/div/div/div[1]/div[1]/div[1]");
+     public static By mostrecent = By.xpath("//*[@id=\"body\"]/div[5]/div/div/div[1]/div[1]/div/div[2]/ul/li[4]");
+     public static By shortlistButton = By.xpath("//*[@id=\"cardid81061849\"]/div/div[1]/div[2]/span[2]");
+     public static By mainshortlistbtn = By.xpath("//*[@id=\"propertysrp\"]/div[1]/div/div/div[2]/div[5]/span");
+     public static By viewShortlistBtn = By.xpath("//*[@id=\"propertysrp\"]/div[1]/div/div/div[2]/div[5]/div/div/a");
+     public static By shortlistedPropertyTitle = By.xpath("//*[@id=\"cardid81061849\"]/div/div[1]/div[2]/h2");
+     public static By shortlistedPropertyCard = By.xpath("//*[@id=\"cardid81061849\"]/div[2]/div[1]/div[2]");
+
+     // New locator for the tab header on the shortlisted page
+     public static By shortlistTabHeader = By.xpath("//*[@id=\"m-tab-Shortlisted\"]");
 }
